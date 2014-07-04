31d8f34beb22638b4f5f50ee8178c50e3745aadc
Scott Robertson <scottymeuk@gmail.com>
2014-07-04 15:29:22 +0100

    Use randomNumber() from faker in strict mode

--- src/Zizaco/FactoryMuff/Kind/Integer.php
+++ src/Zizaco/FactoryMuff/Kind/Integer.php
@@ -9,17 +9,6 @@ class Integer extends Kind
     public function generate()
     {
         $length = (int) $this->getOption(0, 5);
-
-        return $this->randomNumber($length);
-    }
-
-    private function randomNumber($length)
-    {
-        $integer = null;
-        for ($i = 0; $i < $length; $i++) {
-            $integer .= mt_rand(1, 9);
-        }
-
-        return (int) $integer;
+        return $this->faker->randomNumber($length, true);
     }
 }
