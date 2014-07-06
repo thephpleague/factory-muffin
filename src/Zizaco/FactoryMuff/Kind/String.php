da363d6bc2c558c71de57ab9a9b1566167e7e3d6
Scott Robertson <scottymeuk@gmail.com>
2014-07-06 04:13:22 +0100

    Use Faker's lexify and str_repeat to generate a string to a specific length

--- src/Zizaco/FactoryMuff/Kind/String.php
+++ src/Zizaco/FactoryMuff/Kind/String.php
@@ -9,11 +9,6 @@ class String extends Kind
     public function generate()
     {
         $length = $this->getOption(0, 10);
-
-        // Generate a large amount of text. The reason for this is that
-        // faker uses a maximum length, and not an exact length. We then substr this
-        $text = str_replace(' ', null, $this->faker->text($length * 3));
-
-        return substr($text, 0, $length);
+        return $this->faker->lexify(str_repeat('?', $length));
     }
 }
