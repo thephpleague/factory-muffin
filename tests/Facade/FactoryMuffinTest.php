28dcd74b89f53456792c878c37d08b55bdcf13b5
Scott Robertson <scottymeuk@gmail.com>
2014-07-21 19:08:10 +0100

    Test Facade::define method

---
+++ tests/Facade/FactoryMuffinTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace League\FactoryMuffin\Test\Facade;
+
+use League\FactoryMuffin\Facade\FactoryMuffin;
+
+class FactoryMuffinTest extends \PHPUnit_Framework_TestCase
+{
+  public function testDefine()
+  {
+    FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
+      'name' => 'string',
+      'active' => 'boolean',
+      'email' => 'email'
+    ));
+
+    $user = FactoryMuffin::create('\League\FactoryMuffin\Test\Facade\User');
+
+    $this->assertInternalType('string', $user->name);
+    $this->assertInternalType('boolean', $user->active);
+    $this->assertContains('@', $user->email);
+  }
+}
+
+class User
+{
+  public function save()
+  {
+    return true;
+  }
+}
