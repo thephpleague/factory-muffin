d40849999efcc478c72830e25904c31d4c25d2ff
Scott Robertson <scottymeuk@gmail.com>
2014-07-23 13:00:55 +0100

    Set definitions path in setUpBeforeClass

--- tests/test/TestCase.php
+++ tests/test/TestCase.php
@@ -6,5 +6,8 @@ use League\FactoryMuffin\Facade\FactoryMuffin;
 
 class TestCase extends \PHPUnit_Framework_TestCase
 {
-
+  public static function setUpBeforeClass()
+  {
+    FactoryMuffin::setPath(dirname(__DIR__) . '/defintions');
+  }
 }
