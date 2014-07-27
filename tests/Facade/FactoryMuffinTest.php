b18da92567c0bbe1450ca46c151bea9667a0b34e
Graham Campbell <graham@mineuk.com>
2014-07-27 19:50:13 +0100

    Fixed indentation

--- tests/Facade/FactoryMuffinTest.php
+++ tests/Facade/FactoryMuffinTest.php
@@ -6,84 +6,84 @@ use League\FactoryMuffin\Facade\FactoryMuffin;
 
 class FactoryMuffinTest extends \PHPUnit_Framework_TestCase
 {
-  public function testDefine()
-  {
-    FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
-      'name' => 'string',
-      'active' => 'boolean',
-      'email' => 'email'
-    ));
+    public function testDefine()
+    {
+        FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
+            'name' => 'string',
+            'active' => 'boolean',
+            'email' => 'email'
+        ));
 
-    $user = FactoryMuffin::create('\League\FactoryMuffin\Test\Facade\User');
+        $user = FactoryMuffin::create('\League\FactoryMuffin\Test\Facade\User');
 
-    $this->assertInternalType('string', $user->name);
-    $this->assertInternalType('boolean', $user->active);
-    $this->assertContains('@', $user->email);
-  }
+        $this->assertInternalType('string', $user->name);
+        $this->assertInternalType('boolean', $user->active);
+        $this->assertContains('@', $user->email);
+    }
 
-  public function testDefineMultiple()
-  {
-    FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\Profile', array(
-      'profile' => 'text',
-    ));
+    public function testDefineMultiple()
+    {
+        FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\Profile', array(
+            'profile' => 'text',
+        ));
 
-    FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
-      'name' => 'string',
-      'active' => 'boolean',
-      'email' => 'email',
-      'profile' => 'factory|\League\FactoryMuffin\Test\Facade\Profile'
-    ));
+        FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
+            'name' => 'string',
+            'active' => 'boolean',
+            'email' => 'email',
+            'profile' => 'factory|\League\FactoryMuffin\Test\Facade\Profile'
+        ));
 
-    $user = FactoryMuffin::create('\League\FactoryMuffin\Test\Facade\User');
+        $user = FactoryMuffin::create('\League\FactoryMuffin\Test\Facade\User');
 
-    $this->assertInternalType('string', $user->name);
-    $this->assertInternalType('boolean', $user->active);
-    $this->assertContains('@', $user->email);
-  }
+        $this->assertInternalType('string', $user->name);
+        $this->assertInternalType('boolean', $user->active);
+        $this->assertContains('@', $user->email);
+    }
 
-  public function testInstance()
-  {
-    FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
-      'name' => 'string',
-      'active' => 'boolean',
-      'email' => 'email'
-    ));
+    public function testInstance()
+    {
+        FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
+            'name' => 'string',
+            'active' => 'boolean',
+            'email' => 'email'
+        ));
 
-    $user = FactoryMuffin::instance('\League\FactoryMuffin\Test\Facade\User');
+        $user = FactoryMuffin::instance('\League\FactoryMuffin\Test\Facade\User');
 
-    $this->assertInternalType('string', $user->name);
-    $this->assertInternalType('boolean', $user->active);
-    $this->assertContains('@', $user->email);
-  }
+        $this->assertInternalType('string', $user->name);
+        $this->assertInternalType('boolean', $user->active);
+        $this->assertContains('@', $user->email);
+    }
 
-  public function testAttributesFor()
-  {
-    FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
-      'name' => 'string',
-      'active' => 'boolean',
-      'email' => 'email'
-    ));
+    public function testAttributesFor()
+    {
+        FactoryMuffin::define('\League\FactoryMuffin\Test\Facade\User', array(
+            'name' => 'string',
+            'active' => 'boolean',
+            'email' => 'email'
+        ));
 
-    $attributes = FactoryMuffin::attributesFor('\League\FactoryMuffin\Test\Facade\User');
+        $attributes = FactoryMuffin::attributesFor('\League\FactoryMuffin\Test\Facade\User');
 
-    $this->assertInternalType('string', $attributes['name']);
-    $this->assertInternalType('boolean', $attributes['active']);
-    $this->assertContains('@', $attributes['email']);
-  }
+        $this->assertInternalType('string', $attributes['name']);
+        $this->assertInternalType('boolean', $attributes['active']);
+        $this->assertContains('@', $attributes['email']);
+    }
 }
 
 class User
 {
-  public function save()
-  {
-    return true;
-  }
+    public function save()
+    {
+        return true;
+    }
 }
 
 class Profile
 {
-  public function save()
-  {
-    return true;
-  }
+    public function save()
+    {
+        return true;
+    }
 }
