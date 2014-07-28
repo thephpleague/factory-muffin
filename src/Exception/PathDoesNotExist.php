15a74b5cdc95095cf6497719391a427107479920
Scott Robertson <scottymeuk@gmail.com>
2014-07-28 17:06:03 +0100

    Path does not exist exception

---
+++ src/Exception/PathDoesNotExist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace League\FactoryMuffin\Exception;
+
+/**
+ * Class PathDoesNotExist.
+ *
+ * @package League\FactoryMuffin\Exception
+ * @author  Zizaco <zizaco@gmail.com>
+ * @author  Scott Robertson <scottymeuk@gmail.com>
+ * @license <https://github.com/thephpleague/factory-muffin/blob/master/LICENSE> MIT
+ */
+class PathDoesNotExist extends \Exception
+{
+    //
+}
