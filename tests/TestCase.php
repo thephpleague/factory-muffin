12605297c2dee1d7fcd1f03d004a6b52b6bffc72
Scott Robertson <scottymeuk@gmail.com>
2014-07-28 17:08:10 +0100

    Load factories on startup of tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,8 @@ use League\FactoryMuffin\Facade\FactoryMuffin;
 
 class TestCase extends \PHPUnit_Framework_TestCase
 {
+    public static function setupBeforeClass()
+    {
+        FactoryMuffin::loadFactories(__DIR__ . '/factories');
+    }
 }
