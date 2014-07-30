f997bb39da42b98fb43df67b894437addac51c1a
Scott Robertson <scottymeuk@gmail.com>
2014-07-30 19:58:47 +0100

    Use call_user_func_array so we can pass options to faker

--- src/Kind/Generic.php
+++ src/Kind/Generic.php
@@ -33,7 +33,7 @@ class Generic extends Kind
 
         // If it fails to call it, it must not be a real thing
         try {
-            return call_user_func_array(array($this->faker, $this->kind), $this->getOptions());
+            return call_user_func_array(array($this->faker, $this->getKind()), $this->getOptions());
         } catch (InvalidArgumentException $e) {
 
         }
