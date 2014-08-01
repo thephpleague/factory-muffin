e4d0097bd658b79a3a66eacc55c2526ffe1c4d1e
Scott Robertson <scottymeuk@gmail.com>
2014-08-01 15:30:54 +0100

    Set the Faker locale in our tests to show how it can be done

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -6,6 +6,7 @@ abstract class AbstractTestCase extends PHPUnit_Framework_TestCase
 {
     public static function setupBeforeClass()
     {
+        FactoryMuffin::setFakerLocale('en_EN');
         FactoryMuffin::loadFactories(__DIR__ . '/factories');
         FactoryMuffin::setSaveMethod('save'); // this is not required, but allows you to modify the method name
     }
