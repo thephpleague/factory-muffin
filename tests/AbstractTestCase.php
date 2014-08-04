de32ffdf2bdc0d7e5d23c323e3eee4170dde3436
Graham Campbell <graham@mineuk.com>
2014-08-04 10:22:41 +0100

    Removed unneeded calls

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -6,14 +6,11 @@ abstract class AbstractTestCase extends PHPUnit_Framework_TestCase
 {
     public static function setupBeforeClass()
     {
-        FactoryMuffin::setSaveMethod('save'); // optional step
-        FactoryMuffin::setFakerLocale('en_EN'); // optional step
         FactoryMuffin::loadFactories(__DIR__ . '/factories');
     }
 
     public static function tearDownAfterClass()
     {
-        FactoryMuffin::setDeleteMethod('delete'); // optional step
         FactoryMuffin::deleteSaved();
     }
 }
