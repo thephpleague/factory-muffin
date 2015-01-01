e85a92944f38dc9f66a51295b81fa1eb6f320cc5
Graham Campbell <graham@mineuk.com>
2015-01-01 00:53:47 +0000

    Updated copyright notices, and changed authors to reflect actual class authors

--- src/FactoryMuffin.php
+++ src/FactoryMuffin.php
@@ -3,13 +3,11 @@
 /*
  * This file is part of Factory Muffin.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Scott Robertson <scottymeuk@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace League\FactoryMuffin;
@@ -28,6 +26,8 @@ use RegexIterator;
  *
  * @author Graham Campbell <graham@mineuk.com>
  * @author Scott Robertson <scottymeuk@gmail.com>
+ * @author Patrick McCarren <patrick@mmsc.io>
+ * @author Zizaco <zizaco@gmail.com>
  */
 class FactoryMuffin
 {
--- tests/FactoryMuffinTest.php
+++ tests/FactoryMuffinTest.php
@@ -3,13 +3,11 @@
 /*
  * This file is part of Factory Muffin.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Scott Robertson <scottymeuk@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 use League\FactoryMuffin\Exceptions\DefinitionNotFoundException;
